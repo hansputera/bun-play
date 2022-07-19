ce2142f80c4206318d13da2323370a2606a46e60
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-19 13:29:35 +0000

    feat(db): create table books, users, and sessions
    Signed-off-by: GitHub <noreply@github.com>

--- src/db.ts
+++ src/db.ts
@@ -7,24 +7,13 @@ export const db = new Database('bunplay.sqlite');
  * @return {Promise<void>}
  */
 export const initDB = async (): Promise<void> => {
-    db.run(`
-        CREATE TABLE IF NOT EXISTS \`users\` (
-            id INTEGER PRIMARY KEY,
-            name VARCHAR(255),
-            email VARCHAR(255),
-            password VARCHAR(255),
-            created_at DATETIME,
-            updated_at DATETIME
-        );
-
-        CREATE TABLE IF NOT EXISTS \`books\` (
-            id INTEGER PRIMARY KEY,
-            title VARCHAR(255),
-            author VARCHAR(255),
-            publisher VARCHAR(255),
-            synopsis TEXT,
-            addedUserId INTEGER,
-            created_at DATETIME,
-            updated_at DATETIME
-    `);
+    db.run(
+        'CREATE TABLE IF NOT EXISTS `users` (id INTEGER PRIMARY KEY, name VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, verified INT(1) DEFAULT `0`, created_at DATETIME, updated_at DATETIME)',
+    );
+    db.run(
+        'CREATE TABLE IF NOT EXISTS `books` (id INTEGER PRIMARY KEY, name VARCHAR(255), author VARCHAR(255), description VARCHAR(255), addedBy INTEGER NOT NULL, created_at DATETIME, updated_at DATETIME)',
+    );
+    db.run(
+        'CREATE TABLE IF NOT EXISTS `sessions` (id INTEGER PRIMARY KEY, user_id INTEGER, token VARCHAR(255), created_at DATETIME, updated_at DATETIME)',
+    );
 };
